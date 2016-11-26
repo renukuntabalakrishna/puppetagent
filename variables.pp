65b4798c0a7796dd1897c8f1f5f999568ef1ec31
renukuntabalakrishna <balakrishna4mf@gmail.com>
2016-11-26 15:04:07 +0530

    added the varibale file

---
+++ variables.pp
@@ -0,0 +1,21 @@
+$owner = 'root'
+$group = 'root'
+$mode = '0644'
+
+$fileContent= " Hi This is managed by Puppet \n The owner is ${owner} \n The root is ${root}"
+file { '/temp.txt':
+    ensure => file,
+    owner  => $owner,
+    group  => $group,
+    mode   => $mode,
+    content=> $fileContent
+}
+
+file { '/mydir':
+    ensure => directory,
+    owner  => $owner,
+    group  => $group,
+    mode   => $mode,
+}
+
+notify { $fileContent: }
